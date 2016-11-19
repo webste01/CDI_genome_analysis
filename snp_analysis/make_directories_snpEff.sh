d637136bfd309f6e27cc663f0d983aade14e50cb
webste01 <elizabeth.webster@icahn.mssm.edu>
2016-11-19 16:24:04 -0500

    snp level analysis

---
+++ snp_analysis/make_directories_snpEff.sh
@@ -0,0 +1,118 @@
+mkdir data/020265
+mkdir data/020326
+mkdir data/020327
+mkdir data/020334
+mkdir data/020382
+mkdir data/020425
+mkdir data/020472
+mkdir data/020475
+mkdir data/020477
+mkdir data/020478
+mkdir data/020479
+mkdir data/020485
+mkdir data/020690
+mkdir data/020691
+mkdir data/020696
+mkdir data/020700
+mkdir data/020707
+mkdir data/020710
+mkdir data/020916
+mkdir data/021018
+mkdir data/021022
+mkdir data/021024
+mkdir data/021025
+mkdir data/022457
+mkdir data/023061
+mkdir data/023065
+mkdir data/023078
+mkdir data/023106
+mkdir data/023147
+mkdir data/023148
+mkdir data/023150
+mkdir data/023161
+mkdir data/023295
+mkdir data/023300
+mkdir data/023621
+mkdir data/023622
+mkdir data/023623
+mkdir data/023624
+mkdir data/023627
+mkdir data/020474
+mkdir data/020481
+mkdir data/020482
+mkdir data/020695
+mkdir data/020709
+mkdir data/020711
+mkdir data/022456
+mkdir data/023064
+mkdir data/023162
+mkdir data/023298
+mkdir data/023299
+mkdir data/023583
+mkdir data/023584
+mkdir data/023585
+mkdir data/023589
+mkdir data/023591
+mkdir data/023625
+mkdir data/023628
+mkdir data/023065
+mkdir data/023622
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020265.gbk data/020265/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020326.gbk data/020326/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020327.gbk data/020327/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020334.gbk data/020334/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020382.gbk data/020382/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020425.gbk data/020425/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020472.gbk data/020472/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020475.gbk data/020475/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020477.gbk data/020477/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020478.gbk data/020478/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020479.gbk data/020479/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020485.gbk data/020485/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020690.gbk data/020690/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020691.gbk data/020691/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020696.gbk data/020696/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020700.gbk data/020700/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020707.gbk data/020707/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020710.gbk data/020710/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_020916.gbk data/020916/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_021018.gbk data/021018/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_021022.gbk data/021022/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_021024.gbk data/021024/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_021025.gbk data/021025/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_022457.gbk data/022457/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023061.gbk data/023061/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023065.gbk data/023065/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023078.gbk data/023078/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023106.gbk data/023106/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023147.gbk data/023147/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023148.gbk data/023148/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023150.gbk data/023150/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023161.gbk data/023161/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023295.gbk data/023295/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023300.gbk data/023300/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023621.gbk data/023621/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023622.gbk data/023622/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023623.gbk data/023623/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023624.gbk data/023624/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpm_c_023627.gbk data/023627/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_020474.gbk data/020474/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_020481.gbk data/020481/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_020482.gbk data/020482/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_020695.gbk data/020695/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_020709.gbk data/020709/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_020711.gbk data/020711/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_022456.gbk data/022456/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023064.gbk data/023064/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023162.gbk data/023162/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023298.gbk data/023298/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023299.gbk data/023299/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023583.gbk data/023583/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023584.gbk data/023584/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023585.gbk data/023585/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023589.gbk data/023589/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023591.gbk data/023591/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023625.gbk data/023625/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00000crpx_c_023628.gbk data/023628/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00001crpm_c_023065.gbk data/023065/genes.gbk
+cp /sc/orga/projects/bashia02c/projects/cdiff/all/gbk/u00001crpm_c_023622.gbk data/023622/genes.gbk
