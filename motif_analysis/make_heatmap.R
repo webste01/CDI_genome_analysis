4d4232917fb3ddae70a16c340b3c80c08811c852
webste01 <elizabeth.webster@icahn.mssm.edu>
2016-11-30 10:58:05 -0500

    Order the columns by motif cluster

--- motif_analysis/make_heatmap.R
+++ motif_analysis/make_heatmap.R
@@ -11,3 +11,7 @@ df = df[,colSums(df) > 1]
 pdf(paste(file,".pdf",sep=""),width=9, height=8)
 motif_hm<-heatmap.2(df,trace="none",col=my_palette,margins=c(12,8),Rowv=TRUE,Colv=TRUE)
 dev.off()
+mo<-as.hclust(motif_hm$colDendrogram)
+ord<-mo$order
+df_ordered<-df[,c(mo$order)]
+write.csv(df_ordered,"ordered_motif_matrix.csv",quote=F)
