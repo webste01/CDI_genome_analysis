56a3f05b1fa42076ce19c5fc0a64f2bb9f9b51f0
Bashir <bashia02@login1.mothra.hpc.mssm.edu>
2016-12-07 14:01:13 -0500

    corrected reverse comlement check and added in direct count filtering

--- motif_analysis/make_motif_matrix.py
+++ motif_analysis/make_motif_matrix.py
@@ -8,12 +8,17 @@ import argparse
 import csv
 
 motif_fofn=sys.argv[1]
-p_keep=sys.argv[2]
+p_keep=float(sys.argv[2])
 out_name=str(sys.argv[3])
+min_genome_presence = int(sys.argv[4])
 motifs=open(out_name + ".csv","w")
 
 tmpset=set()
-rcDict = dict(zip('ACGTWSMKRYBDHVNacgtwsmkrybdhvn-','TGCAWSKMYRVHDBNtgcawskmyrvhdbn-'))
+from collections import Counter
+motifCounter = Counter()
+genomeMotifs = {} # dictionary of genome ids with their list of motifs
+
+rcDict = dict(zip('ACGTWSMKRYBDHVNacgtwsmkrybdhvn-"','TGCAWSKMYRVHDBNtgcawskmyrvhdbn-"'))
 def revcomp(seq):
    """Return the reverse complement of a string:
 
@@ -34,38 +39,54 @@ with open (motif_fofn) as m:
 		with open (f2,'rb') as f:
 			motif_reader= csv.reader(f,skipinitialspace=True)
 			next(motif_reader)
-			motifs.write(f2.split('/')[2])
+                        genome_id = f2.split('/')[2] # name of geneme
+			motifs.write(genome_id)
+                        genomeMotifs[genome_id] = set()
+                        
 			motifs.write(",")
-			tmpset=set()
 			for row in motif_reader:
-				if row[3]> p_keep:
-					if revcomp(row[0]) not in tmpset:
-						tmpset.add(row[0])
-						motifs.write(row[0])
-						motifs.write(",")
+                           motif = row[0]
+                           rcmotif = revcomp(motif)
+                           p_methylated = float(row[3])
+                           if p_methylated > p_keep:
+                              if motif in genomeMotifs[genome_id] or rcmotif in genomeMotifs[genome_id]:
+                                 # we don't want to add it in twice!
+                                 pass
+                              elif rcmotif not in tmpset:
+                                 tmpset.add(motif)
+                                 motifs.write(motif)
+                                 motifs.write(",")
+                                 motifCounter[motif] += 1
+                                 genomeMotifs[genome_id].add(motif)
+                              else:
+                                 motifs.write(rcmotif)
+                                 motifCounter[rcmotif] += 1
+                                 genomeMotifs[genome_id].add(motif)
+                                 motifs.write(",")
 			motifs.write('\n')
 motifs.close()		
-id_mots = {}
-all_mots = []
-motset = set()
-motifs=open(out_name + ".csv","r")
-for l in motifs:
-	ll = l.strip().split(',')
-	id = ll[0]
-	mot = ll[1:]
-	for motif in mot:
-		all_mots.append(motif)	
-		motset.add(motif)
-	id_mots.setdefault(id,[]).append(mot)
-
-motset_list = list(motset)
 motif_matrix=open(out_name + "_motif_matrix.csv","w")
-motif_matrix.write("%s %s\n" % ("id,",motset_list))
-for id in id_mots:
-	binary = [0] * len(motset)
-	motif = id_mots[id]
-	motif1=motif[0]
-	for pos in range(len(motif1)):
-		index_of_motif = motset_list.index(motif1[pos])
-		binary[index_of_motif]=1
-	motif_matrix.write("%s, %s\n" % (id,binary))
+
+# determine how many genomes are in our table
+n = 0
+motset_list = []
+for motif, count in motifCounter.items():
+   if count >= min_genome_presence:
+      n += 1
+      motset_list.append(motif)
+      
+
+motif_matrix.write("%s %s\n" % ("id,",", ".join(motset_list)))
+
+for genome in genomeMotifs:
+	binary = [0] * n
+        i = 0
+        for motif in motset_list:
+           if motif in genomeMotifs[genome]:
+              binary[i] = 1
+           i += 1
+           binary_str = ", ".join(map(str, binary))
+	motif_matrix.write("%s, %s\n" % (genome,binary_str))
+
+for motif in motset_list:
+   print >>sys.stderr, motif, motifCounter[motif]
