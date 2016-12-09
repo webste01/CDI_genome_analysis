cc3387e046c19910cfa2d9083c259bc59ba8a76d
webste01 <elizabeth.webster@icahn.mssm.edu>
2016-12-09 14:29:09 -0500

    Remove redundant motifs, allow specification of min number of genomes for a motif to be present in

--- motif_analysis/make_heatmap.R
+++ motif_analysis/make_heatmap.R
@@ -1,13 +1,11 @@
 library(gplots)
 args <- commandArgs(TRUE)
 file<-args[1]
-motmat<-read.table(paste(file,"_motif_matrix.csv",sep=""),,header=T)
+motmat<-read.csv(paste(file,"_motif_matrix.csv",sep=""),,header=T)
 df<-data.matrix(motmat)
 rownames(df)<-df[,1]
-df<-df[,-c(1,2)]
+df<-df[,-c(1)]
 my_palette <- colorRampPalette(c("lightgrey","cornflowerblue"))
-#Remove singletons
-df = df[,colSums(df) > 1]
 pdf(paste(file,".pdf",sep=""),width=9, height=8)
 motif_hm<-heatmap.2(df,trace="none",col=my_palette,margins=c(12,8),Rowv=TRUE,Colv=TRUE)
 dev.off()
--- motif_analysis/make_motif_heatmap.sh
+++ motif_analysis/make_motif_heatmap.sh
@@ -1,11 +1,8 @@
 FOFN=$1
 PKEEP=$2
 OUT=$3
-python make_motif_matrix.py ${FOFN} ${PKEEP} ${OUT}
+MIN=$4
+python make_motif_matrix.py ${FOFN} ${PKEEP} ${OUT} ${MIN}
 
-sed 's:[['',]::g' ${OUT}_motif_matrix.csv  > tmp
-sed 's:[]]::g' tmp > tmp2
-mv tmp2 ${OUT}_motif_matrix.csv
-rm tmp
 
 Rscript make_heatmap.R ${OUT}
